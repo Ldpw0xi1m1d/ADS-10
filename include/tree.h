c90442b5fe2fc338908b41e66331f4612d4ef6ff
Ldpw0xi1m1d <99180755+Ldpw0xi1m1d@users.noreply.github.com>
2022-06-08 14:20:36 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -55,7 +55,7 @@ class Tree {
           }
 
  public:
-        void Tree(std::vector<char> node) {
+        explicit Tree(std::vector<char> node) {
                 root = new Pnode;
                 root->val = '*';
                 root->list = create(node, root);
