dd7f5a7e26b3624554fd2b8b33dc812afe11073a
Ldpw0xi1m1d <99180755+Ldpw0xi1m1d@users.noreply.github.com>
2022-06-08 13:53:16 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -8,64 +8,63 @@
 #include <string>
 
 class Ptree {
-private:
+ private:
 	struct Pnode {
-		char val;
-		std::vector <Pnode> list;
-	};
+                char val;
+                std::vector <Pnode> list;
+        };
 
-	Pnode* root = nullptr;
-	int main = 0;
-	std::vector<char> various;
-	int current = 0;
+        Pnode* root = nullptr;
+        int main = 0;
+        std::vector<char> various;
+        int current = 0;
 
-	std::vector<char> deleteParts(std::vector<char> arr, char val) {
-		std::vector<char> line;
-		for (int i = 0; i < arr.size(); i++) {
-			if (arr[i] != val) {
-				line.push_back(arr[i]);
-			}
-		}
-		return line;
-	}
+       std::vector<char> deleteParts(std::vector<char> arr, char val) {
+                std::vector<char> line;
+                for (int i = 0; i < arr.size(); i++) {
+                         if (arr[i] != val)
+                            line.push_back(arr[i]);
+                }
+                return line;
+        }
 
-	std::vector<Pnode> create(std::vector<char> node, Pnode* top) {
-		std::vector<Pnode> temp;
-		for (int i = 0; i < node.size(); i++) {
-			Pnode* tmp = new Pnode;
-			tmp->val = node[i];
-			tmp->list = create(deleteParts(node, node[i]), tmp);
-			temp.push_back(*tmp);
-		}
-		return temp;
-	}
+        std::vector<Pnode> create(std::vector<char> node, Pnode* top) {
+                std::vector<Pnode> temp;
+                for (int i = 0; i < node.size(); i++) {
+                        Pnode* tmp = new Pnode;
+                        tmp->val = node[i];
+                        tmp->list = create(deleteParts(node, node[i]), tmp);
+                        temp.push_back(*tmp);
+                }
+                return temp;
+        }
 
-	std::string getElements(Pnode* cur, std::string target) {
-		if (cur->val != '*') target = target + cur->val;
-		for (int i = 0; i < cur->list.size(); i++) 
-			getElements(&cur->list[i], target);
-		if (target.length() == root->list.size() && current == main - 1 && various.empty()) {
-			for (int i = 0; i < target.length(); i++) various.push_back(target[i]);
-		}
-		else if (target.length() == root->list.size()) {
-			current++;
-		}
-		return target;
-}
+         std::string getElements(Pnode* cur, std::string target) {
+                if (cur->val != '*') target = target + cur->val;
+                for (int i = 0; i < cur->list.size(); i++)
+                         getElements(&cur->list[i], target);
+                if (target.length() == root->list.size() && current == main - 1 && various.empty()) {
+                       for (int i = 0; i < target.length(); i++) various.push_back(target[i]);
+                }
+                else if (target.length() == root->list.size()) {
+                         current++;
+                }
+                return target;
+          }
 
-public:
-	explicit Tree(std::vector<char> node) {
-		root = new Pnode;
-		root->val = '*';
-		root->list = create(node, root);
-	}
-	
-	std::vector<char> get(int i) {
-		main = i;
-		getElements(root, "");
-		std::vector<char> result = various;
-		various.clear();
-		current = 0;
+ public:
+        void Tree(std::vector<char> node) {
+                root = new Pnode;
+                root->val = '*';
+                root->list = create(node, root);
+        }
+
+        std::vector<char> get(int i) {
+                main = i;
+                getElements(root, "");
+                std::vector<char> result = various;
+                various.clear();
+                current = 0;
 		return result;
 	}
 };
